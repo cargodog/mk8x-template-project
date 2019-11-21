23873632605b2b29432e38b2c2d0a00f0cd541cb
cargodog <cargodog@protonmail.com>
2019-11-21 00:32:27 -0500

    Move timer logic into timer_utils.c
    Signed-off-by: cargodog <cargodog@protonmail.com>

--- src/main.c
+++ src/main.c
@@ -1,28 +1,24 @@
 #include "board.h"
 #include "printf.h"
 #include "microcli.h"
-#include "fsl_clock.h"
+#include "timer_utils.h"
 
 
 #define CMD_ENTRY(fn, help) {fn, #fn, help}
 
 int help(MicroCLI_t * ctx, const char * args);
-int history(MicroCLI_t * ctx, const char * args);
-int poke(MicroCLI_t * ctx, const char * args);
-int startBlinking(MicroCLI_t * ctx, const char * args);
-int stopBlinking(MicroCLI_t * ctx, const char * args);
 int ledOff(MicroCLI_t * ctx, const char * args);
 int ledOn(MicroCLI_t * ctx, const char * args);
+int startBlinking(MicroCLI_t * ctx, const char * args);
+int stopBlinking(MicroCLI_t * ctx, const char * args);
 
 MicroCLI_t dbg;
 const MicroCLICmdEntry_t cmdTable[] = {
-    CMD_ENTRY(help, "Print this help message"),
-    CMD_ENTRY(history, "Dump contents MicroCLI history"),
-    CMD_ENTRY(ledOff, "Turn off the LED"),
-    CMD_ENTRY(ledOn, "Turn on the LED"),
-    CMD_ENTRY(poke, "Poke the poor circuit"),
-    CMD_ENTRY(startBlinking, "Start blinking the LED"),
-    CMD_ENTRY(stopBlinking, "Stop blinking the LED"),
+    CMD_ENTRY(help,             "Print this help message"),
+    CMD_ENTRY(ledOff,           "Turn off the LED"),
+    CMD_ENTRY(ledOn,            "Turn on the LED"),
+    CMD_ENTRY(startBlinking,    "Start blinking the LED"),
+    CMD_ENTRY(stopBlinking,     "Stop blinking the LED"),
 };
 const MicroCLICfg_t dbgCfg = {
     .bannerText = "\r\n\n\n\nDemo CLI!\r\n\n",
@@ -35,39 +31,20 @@ const MicroCLICfg_t dbgCfg = {
 bool blinkEnable = true;
 
 
-uint32_t ms_to_ticks(uint32_t ms)
-{
-    uint32_t ticks = CLOCK_GetCoreSysClkFreq();
-    return ms * (ticks / 1000);
-}
-
-void delay_ms(const uint32_t ms)
-{
-    static const uint32_t loopOverheadTicks = 19;
-    volatile uint32_t sysTickCounter = ms_to_ticks(ms);
-    while (sysTickCounter > loopOverheadTicks) {
-        sysTickCounter -= loopOverheadTicks;
-    }
-}
-
 int help(MicroCLI_t * ctx, const char * args)
 {
     return microcli_help(&dbg);
 }
 
-int history(MicroCLI_t * ctx, const char * args)
+int ledOff(MicroCLI_t * ctx, const char * args)
 {
-    MicroCLIHistoryEntry_t * curr = dbg.history;
-    while(curr && curr->str) {
-        microcli_log(&dbg, "%s\n\r", curr->str);
-        curr = curr->older;
-    }
+    board_status_led_off();
     return 0;
 }
 
-int poke(MicroCLI_t * ctx, const char * args)
+int ledOn(MicroCLI_t * ctx, const char * args)
 {
-    microcli_log(&dbg, "ouch!\n\r");
+    board_status_led_on();
     return 0;
 }
 
@@ -80,18 +57,6 @@ int startBlinking(MicroCLI_t * ctx, const char * args)
 int stopBlinking(MicroCLI_t * ctx, const char * args)
 {
     blinkEnable = false;
-    return 0;
-}
-
-int ledOff(MicroCLI_t * ctx, const char * args)
-{
-    board_status_led_off();
-    return 0;
-}
-
-int ledOn(MicroCLI_t * ctx, const char * args)
-{
-    board_status_led_on();
     return 0;
 }
 
@@ -119,9 +84,13 @@ int main(void)
     microcli_init(&dbg, &dbgCfg);
     microcli_banner(&dbg);
     microcli_interpret_string(&dbg, "help", false);
+
     while(1) {
-        delay_ms(TICK_PERIOD_MS);
+        // Tick each sub-process
         microcli_interpreter_tick(&dbg);
         blink_tick(TICK_PERIOD_MS);
+
+        // Delay
+        delay_ms(TICK_PERIOD_MS);
     }
 }
